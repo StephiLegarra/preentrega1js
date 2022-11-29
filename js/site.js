93c2b19736554df55e09e3bf6bb140c6032b21ff
Stephanie Legarra <stephanielegarra@gmail.com>
2022-11-29 19:13:18 -0300

    arreglo js

--- js/site.js
+++ js/site.js
@@ -1,14 +1,15 @@
 // SALUDO BIENVENIDA
-alert("Hola! Bievenid@ a nuestra calculadora de descuentos de TARJETA MAXIUM XII");
+alert("Hola! Bievenid@ a nuestra calculadora de descuentos de CUENTA DNI");
 
 // IDENTIFICACION
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
 alert("Hola " + nombre + " " + apellido);
-console.log("Hola " + nombre + " " + apellido + ", gracias por utilizar nuestro sistema de calculo de descuentos de TARJETA MAXIUM XII");
+console.log("Hola " + nombre + " " + apellido + ", gracias por utilizar nuestro sistema de calculo de descuentos de CUENTA DNI");
 
-// CONSULTA SI TIENE CUENTA 
-let respuesta = prompt(nombre + " ,tienes la TARJETA MAXIUM XII?").toLowerCase();
+// CONSULTA SI TIENE CUENTA DNI
+let respuesta = prompt(nombre + " ,tienes cuenta DNI del Banco Provincia?").toLowerCase();
+
 
 function mesConsulta() {
     let mes = prompt("De que mes quieres consultar los descuentos?").toLowerCase();
@@ -16,48 +17,48 @@ function mesConsulta() {
 
     switch (mes) {
         case "enero":
-            alert("El descuento era del 20%");
-            return (0.20);
+            alert("El descuento era del 30%");
+            return 0.30;
             break;
         case "febrero":
             alert("El descuento era del 35%");
-            return (0.35);
+            return 0.35;
             break;
         case "marzo":
-            alert("El descuento era del 15%");
-            return (0.15);
+            alert("El descuento era del 30%");
+            return 0.30;
             break;
         case "abril":
-            alert("El descuento era del 30%");
-            return (0.30);
+            alert("El descuento era del 35%");
+            return 0.35;
             break;
         case "mayo":
             alert("El descuento era del 40%");
-            return (0.40);
+            return 0.40;
             break;
         case "junio":
             alert("El descuento era del 35%");
-            return (0.35);
+            return 0.35;
             break;
         case "julio":
-            alert("El descuento era del 25%");
-            return (0.25);
+            alert("El descuento era del 40%");
+            return 0.40;
             break;
         case "agosto":
-            alert("El descuento era del 20%");
-            return (0.20);
+            alert("El descuento era del 40%");
+            return 0.40;
             break;
         case "septiembre":
             alert("El descuento era del 45%");
-            return (0.45);
+            return 0.45;
             break;
         case "octubre":
-            alert("El descuento era del 35%");
-            return (0.35);
+            alert("El descuento era del 40%");
+            return 0.40;
             break;
         case "noviembre":
-            alert("El descuento es del 15%");
-            return (0.15);
+            alert("El descuento es del 45%");
+            return 0.45;
             break;
         case "diciembre":
             alert("Lo siento, esta información aún no está definida");
@@ -72,19 +73,18 @@ function mesConsulta() {
     console.log(mes);
 }
 
-// SI TIENE CUENTA 
+// SI TIENE CUENTA DNI
 function preguntar() {
     if (respuesta === "si") {
         alert(`Excelente ${nombre} ${apellido} es algo para aprovechar!`);
         console.log("Continuemos entonces");
 
         mesConsulta();
-
+          
         let monto = parseInt(prompt("cuanto dinero gastaste?"));
         console.log(monto);
         alert("Usted informa haber gastado $" + monto);
         let descuento = mesConsulta();
-        console.log (descuento);
         let resultado = monto * descuento;
         alert("El descuento es de $" + resultado);
         console.log(resultado);
@@ -97,38 +97,36 @@ function preguntar() {
             let monto = parseInt(prompt("cuanto dinero gastaste?"));
             console.log(monto);
             alert("Usted informa haber gastado $" + monto);
-            let descuento = mesConsulta();
-            console.log(descuento);
             let resultado = monto * descuento;
-            alert("El descuento es de $" + resultado);
+            alert("El descuento es de " + resultado);
             console.log(resultado);
             seguir = prompt("Quiere consultar otro descuento?").toLowerCase();
 
-            let terminar = prompt("Si quiere terminar escriba SALIR").toLowerCase();
-            alert(terminar);
-            console.log(terminar);
-            while (terminar != "salir") {
-                alert("Palabra incorrecta");
-                terminar = prompt("Si quiere terminar escriba SALIR").toLowerCase();
-            }
-        }
-
-
-        // SI NO TIENE CUENTA 
-    } else if (respuesta === "no") {
-        alert(`Que lastima ${nombre} ${apellido} es algo para aprovechar!`);
-        console.log("Piensa en hacerte una para poder aprovechar distintos beneficios");
-        let crear = prompt("No te gustaría hacerte una cuenta? es gratis").toLowerCase();
-        console.log(crear); {
-            if (crear === "si") {
-                alert(`Excelente ${nombre}, puedes descargarla desde el play store y hacerte un usuario en tan solo minutos`);
-                console.log("Nos vemos " + nombre + ", aguardaremos aquí y te esperamos cuando tengas la cuenta creada para que puedas consultar por los descuentos");
-            } else if (crear === "no") {
-                alert(`Uh! que lastima! ${nombre}, esperamos que cambies de opinion pronto! Saludos!`);
-                console.log("Adios " + nombre + " ,esperamos verte pronto");
-            }
+        let terminar = prompt("Si quiere terminar escriba SALIR").toLowerCase();
+        alert(terminar);
+        console.log(terminar);
+        while (terminar != "salir") {
+            alert("Palabra incorrecta");
+            terminar = prompt("Si quiere terminar escriba SALIR").toLowerCase();
         }
     }
+
+
+    // SI NO TIENE CUENTA DNI
+} else if (respuesta === "no") {
+    alert(`Que lastima ${nombre} ${apellido} es algo para aprovechar!`);
+    console.log("Piensa en hacerte una para poder aprovechar distintos beneficios");
+    let crear = prompt("No te gustaría hacerte una cuenta? es gratis").toLowerCase();
+    console.log(crear); {
+        if (crear === "si") {
+            alert(`Excelente ${nombre}, puedes descargarla desde el play store y hacerte un usuario en tan solo minutos`);
+            console.log("Nos vemos " + nombre + ", aguardaremos aquí y te esperamos cuando tengas la cuenta creada para que puedas consultar por los descuentos");
+        } else if (crear === "no") {
+            alert(`Uh! que lastima! ${nombre}, esperamos que cambies de opinion pronto! Saludos!`);
+            console.log("Adios " + nombre + " ,esperamos verte pronto");
+        }
+    }
+}
 }
 
 preguntar();
